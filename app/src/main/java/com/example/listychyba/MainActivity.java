a2f1c35ebdbb9555241695910f239fe637ded662
anastv4 <anastazjafil.163@gmail.com>
2026-03-23 16:04:18 +0100

    na 5

--- app/src/main/java/com/example/listychyba/MainActivity.java
+++ app/src/main/java/com/example/listychyba/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.listychyba;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arrayListJezyki;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       arrayListJezyki = new ArrayList<>();
+       arrayListJezyki.add("JavaScript");
+       arrayListJezyki.add("Rust");
+       arrayListJezyki.add("Ruby");
+       arrayListJezyki.add("Kotlin");
+       arrayListJezyki.add("GBScript");
+
+       arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayListJezyki);
+
+       listView = findViewById(R.id.ListViewJezyki);
+       listView.setAdapter(arrayAdapter);
+
+       listView.setOnItemClickListener(
+               new AdapterView.OnItemClickListener() {
+                   @Override
+                   public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                       //view.setBackgroundColor(Color.GREEN);
+                       arrayListJezyki.remove(i);
+                       arrayAdapter.notifyDataSetChanged();
+                   }
+               }
+       );
+       editText = findViewById(R.id.editTextText);
+       button = findViewById(R.id.button);
+
+       button.setOnClickListener(
+               new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       String jezyk = editText.getText().toString();
+                       arrayListJezyki.add(jezyk);
+                       arrayAdapter.notifyDataSetChanged();
+                   }
+               }
+       );
+       //liczba liczb albo stringow
+        //jezeli lista intow (wylosowac liste)
+
     }
 }
